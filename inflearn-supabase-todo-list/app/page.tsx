9e10d9f0a5ecefcde88ec8480b9bf47802b7ace4
develpsh <bshyun1199@gmail.com>
2024-07-29 08:35:35 +0900

    2024.07.29 Section2 #2

--- inflearn-supabase-todo-list/app/page.tsx
+++ inflearn-supabase-todo-list/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import UI from "./ui";
 
 export default function Home() {
-  return <main>TODO LIST
-    <i className="fas fa-check"></i>
+  return <main>
+    <UI/>
   </main>
 }
---
+++ inflearn-supabase-todo-list/app/ui.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Input } from "@material-tailwind/react";
+
+export default function UI() {
+  return (
+    <div className="w-2/3 mx-auto flex flex-col items-center py-10">
+      <h1 className="text-xl">TODO LIST</h1>
+
+      <Input 
+        label="Search TODO" 
+        icon={<i className="fas fa-search" />} 
+      />
+    </div>
+  );
+}
